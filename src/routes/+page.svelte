9be2d691aabecd0b2e0436f151efa747ac7d6f81
HyperAlch <Daemonleak@protonmail.com>
2024-02-08 19:24:24 -0500

    Home page UI & Item API page UI done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header>
+	<hgroup>
+		<h1>Raven Dawn API Lookup Tool</h1>
+		<p>Created by code monkey <b><i>Debian</i></b>, for the Wiki team</p>
+		<img
+			src="https://media.npr.org/assets/img/2013/12/10/istock-18586699-monkey-computer_wide-b549011bc9eacc7431d89ca042874c1e9950c1ac-s1400-c100.jpg"
+			alt="Code monkey"
+			width="50%"
+		/>
+	</hgroup>
+	<nav>
+		<ul>
+			<li>
+				<details role="list">
+					<summary aria-haspopup="listbox">Select an api</summary>
+					<ul role="listbox">
+						<li><a href="/api/items">Items</a></li>
+					</ul>
+				</details>
+			</li>
+		</ul>
+	</nav>
+</header>
---
+++ src/routes/api/items/+page.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+	let searchByName: boolean = true;
+</script>
+
+<header>
+	<h1>Item API</h1>
+	<fieldset>
+		<legend><strong>Change Search Method</strong></legend>
+		<label for="switch-1">
+			<input
+				type="checkbox"
+				id="switch-1"
+				name="switch-1"
+				role="switch"
+				bind:checked={searchByName}
+			/>
+			{#if searchByName}
+				<code>Item Name</code>
+			{:else}
+				<code>Item ID</code>
+			{/if}
+		</label>
+	</fieldset>
+	<input type="search" id="search" name="search" placeholder="Search" />
+</header>
+
+<main>
+	<section id="tables">
+		<figure>
+			<table role="grid">
+				<thead>
+					<tr>
+						<th scope="col">ID</th>
+						<th scope="col">Name</th>
+						<th scope="col">Description</th>
+						<th scope="col">Image</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="row">29974</th>
+						<td>Infusion</td>
+						<td>Used for infusions.</td>
+						<td
+							><img
+								src="https://cdn-api.ravendawn.online/assets/infusion-hejObATe.png"
+								alt="Infusion"
+							/></td
+						>
+					</tr>
+					<tr>
+						<th scope="row">37290</th>
+						<td>Warband Infusion</td>
+						<td>Used for infusions.</td>
+						<td
+							><img
+								src="https://cdn-api.ravendawn.online/assets/warband_infusion-YV6Hkyeq.png"
+								alt="Warband Infusion"
+							/></td
+						>
+					</tr>
+					<tr>
+						<th scope="row">37291</th>
+						<td>Ancient Infusion</td>
+						<td>Used for infusions.</td>
+						<td
+							><img
+								src="https://cdn-api.ravendawn.online/assets/ancient_infusion-9V6ocsd1.png"
+								alt="Ancient Infusion"
+							/></td
+						>
+					</tr>
+				</tbody>
+			</table>
+		</figure>
+	</section>
+</main>
+
+<style>
+	h1,
+	fieldset {
+		text-align: center;
+	}
+
+	legend {
+		padding-bottom: 4px;
+	}
+</style>
