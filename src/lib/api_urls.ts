49d035c0eedffbc0aa8e45e2d8d02cccd575a74c
HyperAlch <Daemonleak@protonmail.com>
2024-02-17 19:12:22 -0500

    Spell & ArcheType API URLs added

--- src/lib/api_urls.ts
+++ src/lib/api_urls.ts
@@ -1,2 +1,5 @@
 const item_api: string = "https://api.ravendawn.online/v1/items";
-export { item_api };
+const spell_api: string = "https://api.ravendawn.online/v1/spells";
+const archetype_api: string = "https://api.ravendawn.online/v1/archetypes";
+
+export { item_api, spell_api, archetype_api };
