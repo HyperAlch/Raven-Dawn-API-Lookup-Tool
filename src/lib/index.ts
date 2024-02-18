48dc25b14d54f4451c7a0f38238d817fad6b8226
HyperAlch <Daemonleak@protonmail.com>
2024-02-17 19:14:46 -0500

    Spell & ArcheType interface types created

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,4 +5,34 @@ interface Item {
     image: string;
 }
 
-export type { Item };
+interface Spell {
+    id: number;
+    channeling: boolean;
+    cooldown: number;
+    archetype: number;
+    range: number;
+    image: string;
+    name: string;
+    description: SpellDescription;
+    cost: SpellCost
+}
+
+interface SpellDescription {
+    format: string;
+    translation: Translation;
+}
+
+interface Translation {
+    "pt-BR": string;
+}
+
+interface SpellCost {
+    mana: Array<number> | null
+}
+
+interface Archetype {
+    id: number,
+    name: string,
+}
+
+export type { Item, Spell, Archetype };
